26cb281de40dacd3a75c01c0065757735013ca83
Erika gonzalez <ejgonzalez971@misena.edu.co>
2016-04-21 16:33:19 -0500

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -525,7 +525,7 @@
     self.spinner.clearsContextBeforeDrawing = NO;
     self.spinner.clipsToBounds = NO;
     self.spinner.contentMode = UIViewContentModeScaleToFill;
-    self.spinner.frame = CGRectMake(self.view.frame.size.width / 2 - 10, self.view.frame.size.height/ 2 - 10, 20, 20);
+    self.spinner.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
 
     self.spinner.hidden = NO;
     self.spinner.hidesWhenStopped = YES;
