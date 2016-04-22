1c4657110d4517e9813c57b943e2abfb7fcc75dd
Erika gonzalez <ejgonzalez971@misena.edu.co>
2016-04-22 14:34:00 -0500

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -526,6 +526,7 @@
     self.spinner.clipsToBounds = NO;
     self.spinner.contentMode = UIViewContentModeScaleToFill;
     self.spinner.frame = CGRectMake(CGRectGetMidX(self.webView.frame), CGRectGetMidY(self.webView.frame), 20.0, 20.0);
+
     self.spinner.hidden = NO;
     self.spinner.hidesWhenStopped = YES;
     self.spinner.multipleTouchEnabled = NO;
