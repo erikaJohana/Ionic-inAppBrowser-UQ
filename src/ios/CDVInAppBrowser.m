2bd8fd4cd150552c6845aa330dde7c4494e18edf
Erika gonzalez <ejgonzalez971@misena.edu.co>
2016-04-26 10:28:04 -0500

    Update CDVInAppBrowser.m

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -566,7 +566,7 @@
     self.addressLabel.alpha = 1.000;
     self.addressLabel.autoresizesSubviews = YES;
     self.addressLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-    self.addressLabel.backgroundColor = [UIColor colorWithRed:0.33 green:0.58 blue:0.28 alpha:1.0];
+    self.addressLabel.backgroundColor = [UIColor colorWithRed:0.00 green:0.35 blue:0.15 alpha:1.0];
     self.addressLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
     self.addressLabel.clearsContextBeforeDrawing = YES;
     self.addressLabel.clipsToBounds = YES;
