0457466136e4a56b2d16746b2936248015e43378
Erika gonzalez <ejgonzalez971@misena.edu.co>
2016-11-08 08:37:00 -0500

    Update InAppBrowser.java
    cambio de color en  toolbar.setBackgroundColor(android.graphics.Color.parseColor("#2EA1D8"));

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -550,7 +550,7 @@ public class InAppBrowser extends CordovaPlugin {
                 // Toolbar layout
                 RelativeLayout toolbar = new RelativeLayout(cordova.getActivity());
                 //Please, no more black!
-                toolbar.setBackgroundColor(android.graphics.Color.parseColor("#007330"));
+                toolbar.setBackgroundColor(android.graphics.Color.parseColor("#2EA1D8"));
                 toolbar.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, this.dpToPixels(44)));
                 toolbar.setPadding(this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2), this.dpToPixels(2));
                 toolbar.setHorizontalGravity(Gravity.LEFT);
