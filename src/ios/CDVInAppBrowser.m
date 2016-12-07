13acf11ca78f31d0e37c7a7ab2bb819a92c542bb
Erika gonzalez <ejgonzalez971@misena.edu.co>
2016-12-07 09:16:06 -0500

    Update CDVInAppBrowser.m
    Cambio color para app gobernación IOS

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -566,7 +566,7 @@
     self.addressLabel.alpha = 1.000;
     self.addressLabel.autoresizesSubviews = YES;
     self.addressLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-    self.addressLabel.backgroundColor = [UIColor colorWithRed:0.2706 green:0.6745 blue:0.8471 alpha:1.0];
+    self.addressLabel.backgroundColor = [UIColor colorWithRed:0.4706 green:0.2039 blue:0.4157 alpha:1.0];
     self.addressLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
     self.addressLabel.clearsContextBeforeDrawing = YES;
     self.addressLabel.clipsToBounds = YES;
